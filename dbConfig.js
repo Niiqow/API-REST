15a75c7cb82ff53e603384f6dbc8677bd21dd339
Nicolas Almonacid <nalmonacidojeda@gmail.com>
2023-02-22 05:39:12 -0300

    dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,9 @@
 const dbConfig = {
-    user: process.env.DB_USER || 'niiqow',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'my-app',
-    password: process.env.DB_PASSWORD || '2212',
-    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
   };
   
   module.exports = dbConfig;
