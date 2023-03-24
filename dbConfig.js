f14ae976d4dab0b14dc643aa06688cf1c897ddc4
Nicolas Santiago Almonacid Ojeda <44521679+Niiqow@users.noreply.github.com>
2023-03-24 11:14:27 -0300

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,9 @@
 const dbConfig = {
-    user: process.env.DB_USER = "myuser",
-    host: process.env.DB_HOST = "localhost",
-    database: process.env.DB_NAME = "my-app",
-    password: process.env.DB_PASSWORD = "123",
-    port: process.env.DB_PORT = "5432",
+    user: process.env.DB_USER || "myuser",
+    host: process.env.DB_HOST || "localhost",
+    database: process.env.DB_NAME || "my-app",
+    password: process.env.DB_PASSWORD || "123",
+    port: process.env.DB_PORT || "5432",
   };
   
 
