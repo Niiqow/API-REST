4f3d4caa55a7936921563e95fba3720730ac2c0e
Nicolas Santiago Almonacid Ojeda <44521679+Niiqow@users.noreply.github.com>
2023-03-24 11:13:04 -0300

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const dbConfig = {
     user: process.env.DB_USER = "myuser",
-    host: process.env.DB_HOST = "lab-dev-postgre001.postgres.database.azure.com",
+    host: process.env.DB_HOST = "localhost",
     database: process.env.DB_NAME = "my-app",
     password: process.env.DB_PASSWORD = "123",
     port: process.env.DB_PORT = "5432",
